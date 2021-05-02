d059230fcbc83a7c8369e759cedc5493d70e1ac6
DRVeyl <DRVeyl@gmail.com>
2021-05-02 12:11:52 -0400

    Upgrade Pipeline
    Handle Thrust Plate
    Linux and CraftManager Popup Warnings

--- Source/ProceduralFairings/UpgradePipeline.cs
+++ Source/ProceduralFairings/UpgradePipeline.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 using SaveUpgradePipeline;
 using UnityEngine;
 
@@ -7,30 +9,33 @@ namespace ProceduralFairings
     [UpgradeModule(LoadContext.SFS | LoadContext.Craft, sfsNodeUrl = "GAME/FLIGHTSTATE/VESSEL/PART", craftNodeUrl = "PART")]
     public class DeprecateAdapterAndResizer : UpgradeScript
     {
-        public override string Name { get => "ProceduralFairings 2.0 PartModule Deprecater";  }
-        public override string Description { get => "Upgrades pre-2.0 ProceduralFairings parts"; }
+        public override string Name { get => "ProceduralFairings 6.0 PartModule Deprecater";  }
+        public override string Description { get => "Upgrades pre-6.0 ProceduralFairings parts"; }
         public override Version EarliestCompatibleVersion { get => new Version(1, 0, 0); }
         public override Version TargetVersion { get => new Version(1,8,1); }
         protected override bool CheckMaxVersion(Version v) => true; // Upgrades are ProcFairings-dependent, not KSP version.
         // protected override TestResult VersionTest(Version v) => true;    // Could also do it this way.
+        private bool popup = false;
 
         public override TestResult OnTest(ConfigNode node, LoadContext loadContext, ref string nodeName)
         {
             nodeName = NodeUtil.GetPartNodeNameValue(node, loadContext);
-            ConfigNode[] nodes = node.GetNodes("MODULE");
             ConfigNode pfBaseNode = node.GetNode("MODULE", "name", "ProceduralFairingBase");
             ConfigNode adapterNode = node.GetNode("MODULE", "name", "ProceduralFairingAdapter");
             ConfigNode resizerNode = node.GetNode("MODULE", "name", "KzFairingBaseResizer");
-            //Debug.Log($"[PF] UpgradePipeline() Context: {loadContext}\nInput node: {node}\nfound Base: {pfBaseNode}\nAdapter: {adapterNode}\nResizer: {resizerNode}");
-            TestResult res = (pfBaseNode is ConfigNode && (adapterNode is ConfigNode || resizerNode is ConfigNode)) ? TestResult.Upgradeable : TestResult.Pass;
-            Debug.Log($"[PF] UpgradePipeline OnTest() context {loadContext} for {nodeName}.  Upgrade requested: {res}");
+            ConfigNode thrustPlateNode = node.GetNode("MODULE", "name", "KzThrustPlateResizer");
+            TestResult res = TestResult.Pass;
+            if (pfBaseNode is ConfigNode)
+                res = adapterNode is ConfigNode || resizerNode is ConfigNode ? TestResult.Upgradeable : res;
+            else if (thrustPlateNode is ConfigNode)
+                res = TestResult.Upgradeable;
             return res;
         }
 
         public override void OnUpgrade(ConfigNode node, LoadContext loadContext, ConfigNode parentNode)
         {
             string nodeName = NodeUtil.GetPartNodeNameValue(node, loadContext);
-            Debug.Log($"[PF] UpgradePipeline OnUpgrade() context {loadContext} nodeName {nodeName}");
+            Debug.Log($"[PF] UpgradePipeline OnUpgrade() context {loadContext} for {nodeName}");
             // When the node is the PART, ParentNode is the Ship
             ConfigNode pfBaseNode = node.GetNode("MODULE", "name", "ProceduralFairingBase");
             if (node.GetNode("MODULE", "name", "ProceduralFairingAdapter") is ConfigNode adapterNode)
@@ -59,6 +64,48 @@ namespace ProceduralFairings
                 Debug.Log($"[PF] Updated ProceduralFairingBase with Resizer data to {pfBaseNode}");
                 node.RemoveNode(resizerNode);
             }
+            if (node.GetNode("MODULE", "name", "KzThrustPlateResizer") is ConfigNode plateNode)
+            {
+                if (pfBaseNode is null)
+                {
+                    pfBaseNode = node.AddNode("MODULE");
+                    pfBaseNode.AddValue("name", "ProceduralFairingBase");
+                }
+                if (plateNode.HasValue("size"))
+                    pfBaseNode.SetValue("baseSize", plateNode.GetValue("size"), true);
+                pfBaseNode.SetValue("mode", $"{Keramzit.ProceduralFairingBase.BaseMode.Plate}", true);
+                Debug.Log($"[PF] Updated ProceduralFairingBase with ThrustPlate data to {pfBaseNode}");
+                node.RemoveNode(plateNode);
+            }
+            if (loadContext == LoadContext.Craft && !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !popup)
+            {
+                popup = true;
+                PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f),
+                                             new Vector2(0.5f, 0.5f),
+                                             "ShowPFUpgradeWarningOS",
+                                             $"{RuntimeInformation.OSDescription} Procedural Fairings Craft Upgrade Warning",
+                                             $"ProceduralFairings has updated your .craft file!  The craft in the VAB may be out of sync with the upgraded file on your {RuntimeInformation.OSDescription} system, and some of your Procedural Fairings may have incorrect settings.  This is expected, please just reload the updated craft file!  Do NOT save the craft before reloading.",
+                                             "OK",
+                                             false,
+                                             HighLogic.UISkin);
+            } else if (loadContext == LoadContext.SFS && !popup && AssemblyLoader.loadedAssemblies.Any(a => a.name.StartsWith("CraftManager")))
+            {
+                GameEvents.onGameStateLoad.Add(DelayedMessage);
+                popup = true;
+            }
+        }
+
+        public void DelayedMessage(ConfigNode _)
+        {
+            PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f),
+                                         new Vector2(0.5f, 0.5f),
+                                         "ShowPFUpgradeWarningCraftManager",
+                                         $"Procedural Fairings / Craft Manager Craft Upgrade Compatibility Warning",
+                                         $"ProceduralFairings has updated your save file, and detected that you are using the Craft Manager mod.  Craft Mananger does not currently call the stock Upgrade Pipeline for .craft files.  This may result in them not upgrading properly to their new settings for Procedural Fairings v6.  Please load each of your craft files once, using the stock loader, to trigger the update.",
+                                         "OK",
+                                         false,
+                                         HighLogic.UISkin);
+            GameEvents.onGameStateLoad.Remove(DelayedMessage);
         }
     }
 }
