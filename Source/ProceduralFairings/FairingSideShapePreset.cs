4e881522ce640d92ecb02d72773411e6cc356436
Pap <papwr17@gmail.com>
2021-07-02 11:02:47 -0500

    Force Merge PFFE to PapEdits (#24)
    Co-authored-by: DRVeyl <DRVeyl@gmail.com>

---
+++ Source/ProceduralFairings/FairingSideShapePreset.cs
@@ -0,0 +1,27 @@
+﻿using Keramzit;
+using UnityEngine;
+
+namespace ProceduralFairings
+{
+    public class FairingSideShapePreset
+    {
+        [Persistent] public string name = "Conic";
+        [Persistent] public Vector4 baseConeShape = new Vector4(0.3f, 0.3f, 0.7f, 0.7f);
+        [Persistent] public Vector4 noseConeShape = new Vector4(0.1f, 0, 0.7f, 0.7f);
+        [Persistent] public int baseConeSegments = 7;
+        [Persistent] public int noseConeSegments = 11;
+        [Persistent] public float noseHeightRatio = 2;
+
+        public void Apply(ProceduralFairingSide side)
+        {
+            side.baseConeShape = baseConeShape;
+            side.noseConeShape = noseConeShape;
+            side.baseConeSegments = baseConeSegments;
+            side.noseConeSegments = noseConeSegments;
+            side.noseHeightRatio = noseHeightRatio;
+            side.ResetNoseCurve();
+            side.ResetBaseCurve();
+            side.rebuildMesh();
+        }
+    }
+}
